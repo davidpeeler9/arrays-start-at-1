e590722cfae6c450801bba77b89e2d268c4786b8
davidpeeler9 <davidpeeler9@gmail.com>
2017-11-14 15:35:49 -0500

    Update windows10.ps1

---
+++ windows10.ps1
@@ -0,0 +1,4 @@
+# https://gist.github.com/alirobe/7f3b34ad89a159e6daa1 -- massive windows 10 script checklist
+# https://docs.google.com/document/d/1SWLaa6Z1RLMxC782YrDw5LPezuN3nwcxV3fdyOf9E5c/edit?usp=sharing -- windows 10 checklist
+# http://hardenwindows10forsecurity.com/
+# https://blogs.technet.microsoft.com/heyscriptingguy/2016/06/27/use-windows-powershell-to-search-for-files/
